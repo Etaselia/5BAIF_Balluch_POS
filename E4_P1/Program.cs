9543f2530999868e6a9bdc3a138d0422a24d4249
Simon Ptak <simon.ptak@outlook.com>
2023-10-04 18:08:23 +0200

    initial commit

---
+++ E4_P1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ExColletions
+{
+    /// <summary>
+    /// TODO: 
+    ///    - Create a constructor to initialize Name, ClassTeacher (KV).
+    ///    - Add a List of students to manage the students in this class.
+    ///    - Use IReadOnlyList for your public property. It should NOT be possible to add or remove students from outside
+    ///      without calling AddStudent or RemoveStudent.
+    ///    - Add a read-only property of type HashSet<string> to get the different cities in this class.
+    /// </summary>
+    
+    class SchoolClass
+    {
+        public string Name { get; }
+        public string ClassTeacher { get; }
+        private List<Student> StudentsActual = new List<Student>();
+        public  IReadOnlyList<Student> Students = new List<Student>();
+        public HashSet<string> Cities = new HashSet<string>();
+
+        public SchoolClass(String name, String classTeacher) {
+            Name = name;
+            ClassTeacher = classTeacher;
+        }
+        /// <summary>
+        /// Adds a student and modifies the schoolclass reference of the provided
+        /// student.
+        /// </summary>
+        public void AddStudent(Student s)
+        {
+            StudentsActual.Add(s);
+            Cities.Add(s.City);
+            Students = StudentsActual.AsReadOnly();
+            s.SchoolClass = this;
+        }
+
+        /// <summary>
+        /// Removes a student and modifies the schoolclass reference of the provided
+        /// student.
+        /// </summary>
+        public void RemoveStudent(Student s)
+        {
+            StudentsActual.Remove(s);
+            Cities.Remove(s.City);
+            Students = StudentsActual.AsReadOnly();
+            s.SchoolClass = null;
+        }
+    }
+
+    /// <summary>
+    /// TODO: 
+    ///    - Add a constructor to initialize the properties Id, Firstname, Lastname and City.
+    ///    - Add a reference to the class of the student (type SchoolClass). This reference is optional,
+    ///      if a student is not assigned to a class is has the value null.
+    ///    - Add an annotation [JsonIgnore] above this property to suppress the content of
+    ///      the class object in your serialized output.
+    /// </summary>
+    class Student
+    {
+        public int Id { get; }
+        public string Lastname { get; }
+        public string Firstname { get; }
+        public string City { get; set; }
+        [JsonIgnore]
+        public SchoolClass SchoolClass { get; set; }
+
+        public Student(int id, String firstname, String lastname, String city) {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            City = city;
+        }
+        /// <summary>
+        /// Updates the reference of the student and adds the student to the new class.
+        /// </summary>
+        /// <param name="k"></param>
+        public void ChangeClass(SchoolClass k) {
+            SchoolClass.RemoveStudent(this);
+            k.AddStudent(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, SchoolClass> classes = new();
+            classes.Add("3AHIF", new SchoolClass(name: "3AHIF", classTeacher: "KV1"));
+            classes.Add("3BHIF", new SchoolClass(name: "3BHIF", classTeacher: "KV2"));
+            classes.Add("3CHIF", new SchoolClass(name: "3CHIF", classTeacher: "KV3"));
+
+            classes["3AHIF"].AddStudent(new Student(id: 1001, firstname: "FN1", lastname: "LN1", city: "CTY1"));
+            classes["3AHIF"].AddStudent(new Student(id: 1002, firstname: "FN2", lastname: "LN2", city: "CTY1"));
+            classes["3AHIF"].AddStudent(new Student(id: 1003, firstname: "FN3", lastname: "LN3", city: "CTY2"));
+            classes["3BHIF"].AddStudent(new Student(id: 1011, firstname: "FN4", lastname: "LN4", city: "CTY1"));
+            classes["3BHIF"].AddStudent(new Student(id: 1012, firstname: "FN5", lastname: "LN5", city: "CTY1"));
+            classes["3BHIF"].AddStudent(new Student(id: 1013, firstname: "FN6", lastname: "LN6", city: "CTY1"));
+
+            Student s = classes["3AHIF"].Students[0];
+            Console.WriteLine($"s sitzt in der Klasse {s.SchoolClass?.Name} mit dem KV {s.SchoolClass?.ClassTeacher}.");
+            Console.WriteLine($"In der 3AHIF sind folgende Städte: {JsonSerializer.Serialize(classes["3AHIF"].Cities)}.");
+
+            Console.WriteLine("3AHIF vor ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3AHIF"].Students));
+            s.ChangeClass(classes["3BHIF"]);
+            Console.WriteLine("3AHIF nach ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3AHIF"].Students));
+            Console.WriteLine("3BHIF nach ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3BHIF"].Students));
+            Console.WriteLine($"s sitzt in der Klasse {s.SchoolClass?.Name} mit dem KV {s.SchoolClass?.ClassTeacher}.");
+        }
+    }
+}
